fdf44910f4dc01d49dd5d4127ff127f8a40f0990
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:32:21 +0300

    Add files via upload

---
+++ Quad/Quad/Program.cs
@@ -0,0 +1,95 @@
+﻿﻿using System;
+
+namespace QuadNS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, toSqrt;
+            Quad quad = new Quad();
+
+            bool num = quad.CheckArgs(args);
+            if (!num)
+                return;
+
+            bool parse = quad.CheckParse(args, out a, out b, out c);
+            if (!parse)
+                return;
+
+            string stringOneSolution = quad.OneSolution(a, b, c);
+            if (stringOneSolution != null)
+                Console.WriteLine(stringOneSolution);
+
+            toSqrt = Math.Pow(b, 2) - 4*a*c;
+            bool boolNoSolution = quad.NoSolution(a, b, c, toSqrt);
+
+            if (toSqrt < 0 || a == 0)
+                return;
+            else
+            {
+                toSqrt = Math.Sqrt(toSqrt);
+                string stringSolution = quad.Solution(a, b, c, toSqrt);
+                Console.WriteLine(stringSolution);
+            }
+            
+        }
+    }
+
+    public class Quad
+        {
+            public  string Solution(double a, double b, double c, double toSqrt)
+            {
+                double x1 = (- b+ toSqrt) /(2*a);
+                double x2 = (-1)*(toSqrt + b)/(2*a);
+                if(x1 != x2)
+                return "X1 = " + x1 + "\nX2 = " + x2;
+                return null;
+            }
+
+            public  bool NoSolution(double a, double b, double c, double toSqrt)
+            {
+                if (!(toSqrt < 0)) return false;
+                Console.WriteLine("no solution");
+                return true;
+            }
+
+            public  string OneSolution(double a, double b, double c)
+            {
+                if (a == 0)
+                     return "X = " + (-1) * c / b;
+                else
+                 {
+                     if (b == 2 * Math.Sqrt(a) * Math.Sqrt(c))
+                         return "X = " + (-b / 2 * a);   
+                 }
+                return null;
+            }
+
+            public  bool CheckArgs(string[] args)
+            {
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("should be 3 arguments");
+                    return false;
+                }
+                return true;
+
+            }
+
+            public  bool CheckParse(string[] args, out double a, out double b, out double c)
+            {
+                if (!double.TryParse(args[0], out a) || !double.TryParse(args[1], out b) ||
+                    !double.TryParse(args[2], out c))
+                {
+                    Console.WriteLine("parsing didn't succseed");
+                    a = 0;
+                    b = 0;
+                    c = 0;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+
---
+++ Quad/UnitTestQuad/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuadNS;
+
+namespace UnitTestQuad
+{
+    [TestClass]
+    public class QuadTests
+    {
+        [TestMethod]
+        public void CheckArgs_WhenCalledWith3ValidParameters_ReturnsTrue()
+        {
+            var quad = new Quad();
+            string[] str = { "1", "10", "2" };
+
+            Assert.AreEqual(true, quad.CheckArgs(str));
+        }
+
+        [TestMethod]
+        public void Test_CheckNot3Args_IsFalse()
+        {
+            var quad = new Quad();
+            string[] str = { "1", "10" };
+            Assert.AreEqual(false, quad.CheckArgs(str));
+        }
+        //_-------------------------------------------------------------_//
+        [TestMethod]
+        public void Test_CheckParseSuccseded_IsTrue()
+        {
+            string[] str = { "1", "2", "3" };
+            var quad = new Quad();
+            double x = 2, y = 8, z = 1;
+
+            Assert.AreEqual(true, quad.CheckParse(str, out x, out y, out z));
+        }
+        [TestMethod]
+        public void Test_CheckParseFaild_IsFalse()
+        {
+            string[] str = { "1", "10", "xxx" };
+            double x = 2, y = 8, z = 1;
+            var quad = new Quad();
+            Assert.AreEqual(false, quad.CheckParse(str, out x, out y, out z));
+        }
+
+        [TestMethod]
+        public void Test_NoSolutionWithNegetiveSqrt_IsTrue()
+        {
+            double toSqrt = -2;
+            double x = 2, y = 8, z = 1;
+            var quad = new Quad();
+            Assert.AreEqual(false, quad.NoSolution(toSqrt, x, y, z));
+        }
+
+
+    }
+}
