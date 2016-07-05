2194609554f86b89ffb51b368826ba46b979d484
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:34:40 +0300

    Add files via upload

---
+++ Strings/StringUnitTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strings;
+
+namespace StringUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Check_ReverseString_IsEqual()
+        {
+            string[] str = new[] { "victor", "kayumov", "was", "here"};
+
+            var splitStr = new ReverseString();
+            
+
+            StringAssert.StartsWith("here was kayumov victor", splitStr.ReverseStr(str));
+        }
+
+        [TestMethod]
+        public void Check_LengthOfString_IsTrue()
+        {         
+            string strLen = "victor kayumov was here";   
+            var len = new Split();
+
+            Assert.AreEqual(4,len.SplitingString(strLen));
+        }
+
+        [TestMethod]
+        public void Check_SortedString_InRightPlace()
+        {
+            string[] str = new[] { "victor", "kayumov", "was", "here" };
+
+            var sortedStr = new Sort();
+
+
+            StringAssert.StartsWith("here kayumov victor was", sortedStr.SortingString(str));
+        }
+
+    }
+
+}
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string str;
+            do
+            {
+                Console.WriteLine("Insert a sentence:");
+                str = Console.ReadLine();
+
+                var split = new Split();
+                split.SplitingString(str);
+                
+            } while (str != "");
+        }
+
+    }
+
+    public class Split
+    {
+        public int SplitingString(string str)
+        {
+
+            if (string.IsNullOrEmpty(str))
+                return 0 ;
+            else
+            {
+                string[] splitStr = Regex.Split(str, " ");
+                foreach (var split in splitStr)
+                {
+                    Console.WriteLine(split);
+                }
+                Console.WriteLine($"The number of words is: {splitStr.Length}");
+
+                var reverse = new ReverseString();
+                Console.WriteLine(reverse.ReverseStr(splitStr));
+                var sort = new Sort();
+                Console.WriteLine(sort.SortingString(splitStr));
+                return splitStr.Length;
+            }
+           
+        }
+       
+    }
+
+    public class ReverseString
+    {
+        public string ReverseStr(string[] str)
+        {
+            Array.Reverse(str);
+            return string.Join(" ", str);
+        }
+    }
+    public class Sort
+    {
+        public string SortingString(string[] str)
+        {
+            Array.Sort(str);
+            return string.Join(" ", str);
+        }
+    }
+
+}
