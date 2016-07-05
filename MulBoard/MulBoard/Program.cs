7d270e30a8f08f4d33f97d9af9b3c7242bbfe047
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:30:34 +0300

    Add files via upload

---
+++ MulBoard/MulBoard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row < 11; row++)
+            {
+                for (int column = 1; column < 11; column++)
+                {
+                    Console.Write(String.Format("{0,3} "+ "|",row*column));
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+
