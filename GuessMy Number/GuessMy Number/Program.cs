5327f46dc772ebfb53e6fb7aa441a217836adca8
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:29:57 +0300

    Add files via upload

---
+++ GuessMy Number/GuessMy Number/Program.cs
@@ -0,0 +1,44 @@
+﻿﻿using System;
+
+namespace GuessMy_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secret = new Random().Next(1, 101);
+            int numOfGusses = 0;
+
+            Console.WriteLine("Enetr a Guess number: ");
+            while (numOfGusses++ < 7)
+            {
+                int i = ParseNumber();
+
+                if (i < secret)
+                    Console.WriteLine("too small");
+                else if (i > secret)
+                    Console.WriteLine("too big");
+                else
+                {
+                    Console.WriteLine("you rigth. The secret number is: {0}", secret);
+                    break;
+                }
+            }
+            if (numOfGusses == 8)
+                Console.WriteLine("you have failed,The secret number is: {0}", secret);
+            Console.ReadLine();
+        }
+
+         public static int ParseNumber()
+         {
+            int i;
+            if(int.TryParse(Console.ReadLine(), out i)==false)
+                throw new Exception("TryParse is failed");
+             return i;
+         }
+
+        
+    }
+}
+
+
---
+++ GuessMy Number/GuessTestNumber/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuessTestNumber
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+      //  [ExpectedException(typeof(Exception))]
+        public void Test_CheckIfTryParseSuccseded_IsTrue()
+        {
+
+        }
+    }
+}
