4edbed31f84ed1147ef6a338ab2d260380b086c1
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:28:58 +0300

    Add files via upload

---
+++ DollarStairs/DollarStairs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DollarStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Enter a character: ");
+                var ch = Console.ReadLine();
+
+                while (number > 0)
+                {
+                    for (int i = number; i > 0; i--)
+                        Console.Write(ch);
+                    Console.WriteLine();
+                    number--;
+                }
+            }           
+       
+        }
+        
+    }
+}
+
+    
