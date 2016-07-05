f91d7e735bea9cc9d9d1b1d7c7ebd20ce6c4ef9d
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:33:12 +0300

    Add files via upload

---
+++ Rationals/Rationals/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Rationals
+{
+    #region Struct
+    public struct Rational
+        {
+
+        public Rational(int numerator, int denominator)
+        {
+            Numenator = numerator;
+            Denomirator = denominator;
+        }
+        public Rational(int numerator)
+        {
+            Numenator = numerator;
+            Denomirator = 1;
+        }
+
+            public int Numenator { get; private set; }
+            public int Denomirator { get; private set; }
+            public double Value { get { return (double)this.Numenator / this.Denomirator; } }
+            
+           
+            public Rational Add(Rational rational)
+            {
+                if (Denomirator == rational.Denomirator) //Checks if the denominator is the same
+                {
+                    var newRationalNumber = new Rational(Numenator + rational.Numenator, Denomirator);
+                    return newRationalNumber;
+                }
+                else
+                {
+                    int denomirator = Denomirator * rational.Denomirator;
+                    var newRationalNumber = new Rational((denomirator / rational.Denomirator) * rational.Numenator
+                        + (denomirator / Denomirator) * Numenator, denomirator);
+                    return newRationalNumber;
+                }
+            }
+
+            public Rational Mul(Rational rational)
+            {
+                var newRationalNumber = new Rational(Numenator * rational.Numenator, Denomirator * rational.Denomirator);
+                return newRationalNumber;
+            }
+
+            public void Reduce()
+            {
+                var numerator = Numenator;
+                 Numenator /= Gcd(Numenator, Denomirator); 
+                 Denomirator /= Gcd(numerator, Denomirator);
+            }
+          
+        // Greatest Common Divisor, from internet
+        public static int Gcd(int a, int b) 
+        {
+            while (true)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+                var r = a % b;
+                a = b;
+                b = r;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Numenator.ToString() + "/" + Denomirator.ToString();
+        }
+
+    }
+    #endregion
+
+
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                var rational = new Rational(7);
+                var rational1 = new Rational(13, 26);
+                var rational2 = new Rational(9, 17);
+                
+
+                var rational3 = rational1.Add(rational2);
+                var rational4 = rational1.Mul(rational2);
+                var rational5 = rational.Add(rational1);
+
+                Console.WriteLine("Calculations of rational numbers\n");
+                Console.WriteLine(rational1.ToString() + " + " + rational2.ToString() + " = " + rational3.ToString() + " = " + rational3.Value);
+                Console.WriteLine(rational1.ToString() + " * " + rational2.ToString() + " = " + rational4.ToString() + " = " + rational4.Value);
+                Console.WriteLine(rational.ToString() + " + " + rational1.ToString() + " = " + rational5.ToString() + " = " + rational5.Value);
+
+
+                 rational1.Reduce();
+                 rational2.Reduce();
+                 rational3.Reduce();
+                 rational4.Reduce();
+                 rational5.Reduce();
+
+                Console.WriteLine("\nAfter reduce rational numbers\n");
+
+                Console.WriteLine(rational1.ToString() + " + " + rational2.ToString() + " = " + rational3.ToString() + " = " + rational3.Value);
+                Console.WriteLine(rational1.ToString() + " * " + rational2.ToString() + " = " + rational4.ToString() + " = " + rational4.Value);
+                Console.WriteLine(rational.ToString() + " + " + rational1.ToString() + " = " + rational5.ToString() + " = " + rational5.Value);
+        }
+    }
+        
+  }
