4533a62c9a90d1bb1b995fcf189fda0206dd8668
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:34:10 +0300

    Add files via upload

---
+++ Shapes/Shapes/Shape.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Text;
+
+namespace ShapeLib
+{
+    public interface IPersist
+    {
+        void Write(StringBuilder sb);
+    }
+
+    public abstract class Shape
+    {
+        public ConsoleColor Color { get; set; }
+
+        public abstract double Area { get; }
+
+        public Shape()//Default constructor
+        {
+            Color = ConsoleColor.White;
+        }
+
+        public Shape(ConsoleColor color)
+        {
+            Color = color;
+        }
+
+        public virtual void Display()
+        {
+            Console.BackgroundColor = Color;
+
+        }
+    }
+    public class Rectangle : Shape, IPersist, IComparable
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+
+
+        public override double Area
+        {
+            get
+            {
+                return Width * Height;
+            }
+        }
+
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"The Width of rectangle is: {Width}");
+            Console.WriteLine($"The Height of rectangle is: {Height}");
+        }
+        //-----------------------------------------------------------------------------------------//
+        public void Write(StringBuilder sb)
+        {
+            sb.Append("Widht: " + Width.ToString() + "\t");
+            sb.Append("Height: " + Height.ToString() + "\t");
+        }
+
+        public int CompareTo(object obj)
+        {
+            Rectangle rec = (Rectangle)obj;
+
+            return this.Area.CompareTo(rec.Area);
+        }
+        //-----------------------------------------------------------------------------------------//
+    }
+    public class Ellipse : Shape, IPersist, IComparable
+    {
+        public int SmallRadius { get; set; }
+        public int BigRadius { get; set; }
+
+        public Ellipse(int smallRadius, int bigRadius)
+        {
+            SmallRadius = smallRadius;
+            BigRadius = bigRadius;
+        }
+        public override double Area
+        {
+            get
+            {
+                Console.WriteLine($"The SmallRadius of Ellipse is: {SmallRadius}");
+                Console.WriteLine($"The BigRadius of Ellipse is: {BigRadius}");
+                return Math.PI * SmallRadius * BigRadius;
+            }
+        }
+
+        public override void Display()
+        {
+
+            base.Display();
+        }
+        //-----------------------------------------------------------------------------------------//
+        public void Write(StringBuilder sb)
+        {
+            sb.Append("SmallRadius: " + SmallRadius.ToString() + "\t");
+            sb.Append("BigRadius: " + BigRadius.ToString() + "\t");
+        }
+
+        public int CompareTo(object obj)
+        {
+            Ellipse ellipse = (Ellipse)obj;
+
+            return this.Area.CompareTo(ellipse.Area);
+        }
+        //-----------------------------------------------------------------------------------------//
+    }
+
+    public class Circle : Ellipse
+    {
+        public Circle(int radius) : base(radius, radius)
+        {
+        }
+        public override double Area
+        {
+            get
+            {
+                return Math.PI * SmallRadius * BigRadius;
+            }
+        }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"The Radius of Circle is: {BigRadius}");
+        }
+    }
+}
---
+++ Shapes/ShapesApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using ShapeLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeManager shapes = new ShapeManager();
+            shapes.Add(new Rectangle(10, 20) { Color = ConsoleColor.Blue });
+            shapes.Add(new Ellipse(5, 7) { Color = ConsoleColor.Red });
+            shapes.Add(new Circle(5));
+            Console.WriteLine($"Count of shapes in this array is: {shapes.Count}\n");
+            shapes.DisplayAll();
+
+            StringBuilder sb = new StringBuilder();
+            shapes.Save(sb);
+            Console.WriteLine(sb.ToString());
+
+            Rectangle rec = new Rectangle(8, 9);
+            Rectangle rec2 = new Rectangle(8, 7);
+            Console.WriteLine("\nWhich rectangle is bigger: " + rec.CompareTo(rec2));
+
+            Ellipse ellipse = new Ellipse(1, 3);
+            Ellipse ellipse2 = new Ellipse(4, 8);
+            Console.WriteLine("Which wllipse is bigger: " + ellipse.CompareTo(ellipse2));
+        }
+    }
+    public class ShapeManager
+    {
+        List<Shape> array = new List<Shape>();
+        public void Add(Shape newShape)
+        {
+            array.Add(newShape);
+        }
+        public void DisplayAll()
+        {
+            foreach (var shape in array)
+            {
+                shape.Display();
+                Console.WriteLine($"The area is : {shape.Area}");
+                Console.WriteLine("-------------------------------------------------");
+            }
+
+        }
+        public void Save(StringBuilder sb)
+        {
+            foreach (var shape in array)
+            {
+                IPersist ipersist = (shape as IPersist);
+                if (ipersist != null)
+                    ipersist.Write(sb);
+            }
+
+        }
+        public Shape this[int index]   // Indexer declaration
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return array.Count;
+            }
+        }
+    }
+}
