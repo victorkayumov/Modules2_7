48fc298f4283375012f6341c757edb17448d5321
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:27:58 +0300

    Add files via upload

---
+++ Calculator/Calc/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Lab2._2
+{
+    public class Calculations
+    {
+        public double Divide(double number1, double number2)
+        {
+
+            if (number2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (number1 / number2);
+        }
+        public double Add(double number1, double number2)
+        {
+            if (number1 + number2 >= Double.MaxValue)
+            {
+                throw new OverflowException();
+            }
+
+            return number1 + number2;
+        }
+        public double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+        public double Substract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+    }
+
+    public class Calculator
+    {
+        private static void Main()
+        {
+            double fNumber;
+            double sNumber;
+            var validOperator = false;
+            Calculations myCalc = new Calculations();
+
+            Console.Write("Insert First Number: ");
+            double.TryParse(Console.ReadLine(), out fNumber);
+            Console.Write("Insert Second Number: ");
+            double.TryParse(Console.ReadLine(), out sNumber);
+            while (validOperator == false)
+            {
+                Console.Write("Insert operator ( + / - * ) that you want: ");
+                var userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "/":
+                        try
+                        {
+                            Console.WriteLine("The Answer is: " + myCalc.Divide(fNumber, sNumber));
+                            if (sNumber == 0)
+                            {
+                                throw new DivideByZeroException();
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Can't Divide By Zero");
+                        }
+                        validOperator = true;
+                        break;
+                    case "+":
+                        try
+                        {
+                            Console.WriteLine("The Answer is: " + myCalc.Add(fNumber, sNumber));
+                            if (fNumber + sNumber >= Double.MaxValue)
+                            {
+                                throw new OverflowException();
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Overflow Exception");
+                        }
+
+                        validOperator = true;
+                        break;
+                    case "*":
+                        Console.WriteLine("The Answer is: " + myCalc.Multiply(fNumber, sNumber));
+                        validOperator = true;
+                        break;
+                    case "-":
+                        Console.WriteLine("The Answer is: " + myCalc.Substract(fNumber, sNumber));
+                        validOperator = true;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Calculator/CalcUnitTest/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab2._2;
+
+
+
+namespace CalcUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+          [ExpectedException(typeof(DivideByZeroException))]
+        public void Calculations_Divide_DivideByZero()
+        {
+            Calculations myCalc = new Calculations();
+            double numerator = 9;
+            double denominator = 0;
+            
+            myCalc.Divide(numerator, denominator);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void Calculations_AddMaxValue_MaxValueOverFlow()
+        {
+            Calculations myCalc = new Calculations();
+            double _num1 = Double.MaxValue;
+            double _num2 = 9;
+            myCalc.Add(_num1, _num2);
+        }
+
+
+        [TestMethod]
+        public void Calculations_Add2Numbers_DoubleNumbers()
+        {
+            Calculations myCalc = new Calculations();
+            double _num1 = 1;
+            double _num2 = 9;
+           
+            Assert.AreEqual(10, myCalc.Add(_num1, _num2));
+        }
+
+        [TestMethod]
+        public void Calculations_Substract2Numbers_DoubleNumbers()
+        {
+            Calculations myCalc = new Calculations();
+            double _num1 = 1;
+            double _num2 = 9;
+
+            Assert.AreEqual(-8, myCalc.Substract(_num1, _num2));
+        }
+
+         [TestMethod]
+        public void Calculations_Multiply2Numbers_DoubleNumbers()
+        {
+            Calculations myCalc = new Calculations();
+            double _num1 = 10;
+            double _num2 = 9;
+
+            Assert.AreEqual(90, myCalc.Multiply(_num1, _num2));
+        }
+
+         [TestMethod]
+        public void Calculations_Divide2Numbers_DoubleNumbers()
+        {
+            Calculations myCalc = new Calculations();
+            double _num1 = 18;
+            double _num2 = 9;
+
+            Assert.AreEqual(2, myCalc.Divide(_num1, _num2));
+        }
+    }
+}
+
+
