8f1b2435419771cc04b9e02d44534978d8ea4e01
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:28:33 +0300

    Add files via upload

---
+++ CustomersApp/CustomersApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CustomersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer[] array = new Customer[6];
+
+            array[0] = new Customer("victor", 1, "karmiel");
+            array[1] = new Customer("dan", 2, "maalot");
+            array[2] = new Customer("lior", 5, "karmiel");
+            array[3] = new Customer("haim", 3, "haifa");
+            array[4] = new Customer("yossi", 4, "maalot");
+            array[5] = new Customer("Dan", 2, "maalot");
+
+            Console.WriteLine("Array of customers:");           
+            Display(array);
+
+            Console.WriteLine("\nSorted array of customers:");
+            Array.Sort(array);
+            Display(array);
+
+            var comparer = new Customer.AnotherCustomerComparer();
+            Array.Sort(array, comparer);
+            Console.WriteLine("\nSorted array by ID:");
+            Display(array);
+        }
+
+        public static void Display(Customer[] array)
+        {
+            foreach (var customers in array)
+            {
+                Console.WriteLine($" Name: {customers.Name}\t Id: {customers.Id}\t Address: {customers.Address}\t");
+            }
+        }
+    }
+  
+
+    public class Customer : IComparable<Customer>, IEquatable<Customer>
+    {
+        public string Name { get; set; }
+        public int Id { get; }
+        public string Address { get; set; }
+
+        public Customer(string name, int id, string address)
+        {
+            Name = name;
+            Id = id;
+            Address = address;
+        }
+
+
+
+        public int CompareTo(Customer other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+
+        public bool Equals(Customer other)
+        {
+            return this.Id.Equals(other.Id) && this.Name.Equals(other.Name);
+
+        }
+
+        public class AnotherCustomerComparer : IComparer<Customer>
+        {
+            public int Compare(Customer customer1, Customer customer2)
+            {
+                return customer1.Id.CompareTo(customer2.Id);
+            }
+        }
+    }
+}
---
+++ CustomersApp/UnitTestCustomersApp/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CustomersApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCustomersApp
+{
+    [TestClass]
+    public class UnitTest1 
+    {
+        [TestMethod]
+        public void Compare_TwoNames_AreEqual()
+        {
+            var customer = new Customer("VICTOR", 2, "maalot");
+
+            Assert.AreEqual(0, customer.Name.ToUpper().CompareTo("victor".ToUpper()));
+        }
+
+        [TestMethod]
+        public void CheckISEqual_TwoNames_IsTrue()
+        {
+            var customer1 = new Customer("victor", 1, "karmiel");
+            var customer2 = new Customer("victor", 2, "maalot");
+
+            Assert.IsTrue(customer1.Name.Equals(customer2.Name));
+        }
+        [TestMethod]
+        public void CheckISEqual_TwoId_IsTrue()
+        {
+            var customer1 = new Customer("victor", 2, "karmiel");
+            var customer2 = new Customer("victor", 2, "maalot");
+
+            Assert.IsTrue(customer1.Id.Equals(customer2.Id));
+        }
+
+        [TestMethod]
+        public void Compare_TwoId_AreEqual()
+        {
+            var customer1 = new Customer("victor", 3, "karmiel");
+            var customer2 = new Customer("victor", 3, "maalot");
+
+            Assert.AreEqual(0, customer1.Id.CompareTo(customer2.Id));
+        }
+    }
+}
