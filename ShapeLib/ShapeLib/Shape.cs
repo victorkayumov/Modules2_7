be9fbe695f6ce45af56e113538a6416722efcc3d
victorkayumov <victormaalot@gmail.com>
2016-07-05 13:33:39 +0300

    Add files via upload

---
+++ ShapeLib/ShapeLib/Shape.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+
+namespace ShapeLib
+{ 
+
+    public abstract class Shape
+    {
+        public ConsoleColor Color { get; set; }
+
+        public abstract double Area { get; }
+
+        public Shape()//Default constructor
+        {
+            Color = ConsoleColor.White;
+        }
+
+        public Shape(ConsoleColor color)
+        {
+            Color = color;
+        }
+
+        public virtual void Display()
+        {
+            Console.BackgroundColor = Color;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area
+        {
+            get
+            {
+                return Width * Height;
+            }
+        }
+       
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"The Width of rectangle is: {Width}");
+            Console.WriteLine($"The Height of rectangle is: {Height}");
+        }
+
+    }
+    public class Ellipse : Shape
+    {
+        public int SmallRadius { get; set; }
+        public int BigRadius { get; set; }
+
+        public Ellipse(int smallRadius, int bigRadius)
+        {
+            SmallRadius = smallRadius;
+            BigRadius = bigRadius;
+        }
+        public override double Area
+        {
+            get
+            {
+                Console.WriteLine($"The SmallRadius of Ellipse is: {SmallRadius}");
+                Console.WriteLine($"The BigRadius of Ellipse is: {BigRadius}");
+                return Math.PI * SmallRadius * BigRadius;
+            }
+        }
+
+        public override void Display()
+        {
+            base.Display();                   
+        }
+    }
+
+    public class Circle : Ellipse
+    {
+        public Circle(int radius):base(radius,radius)
+        {          
+        }
+        public override double Area
+        {
+            get
+            {
+                return Math.PI * SmallRadius * BigRadius;
+            }
+        }
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"The Radius of Circle is: {BigRadius}");
+        }
+    }
+}
---
+++ ShapeLib/ShapesApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using ShapeLib;
+using System;
+using System.Collections.Generic;
+
+
+namespace ShapesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeManager shapes = new ShapeManager();
+            shapes.Add(new Rectangle(10, 20));
+            shapes.Add(new Ellipse(5, 7) { Color = ConsoleColor.Blue });
+            shapes.Add(new Circle(5) { Color = ConsoleColor.Red });
+            Console.WriteLine($"Count of shapes in this array is: {shapes.Count}\n");
+            shapes.DisplayAll();
+        }
+    }
+    public class ShapeManager
+    {
+        List<Shape> array = new List<Shape>();
+        public void Add(Shape newShape)
+        {
+            array.Add(newShape);
+        }
+        public void DisplayAll()
+        {
+            foreach (var shape in array)
+            {
+                shape.Display();
+                Console.WriteLine($"The area is : {shape.Area}");
+            }
+
+        }
+        public Shape this[int index]   // Indexer declaration
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+        public int Count { get { return array.Count; } }
+    }
+}
