3c26eed877c3ea420dab969b9812711dff61d50e
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-12 17:10:40 +0300

    Lab 2.7

--- DollarStairs/DollarStairs/Program.cs
+++ DollarStairs/DollarStairs/Program.cs
@@ -8,16 +8,20 @@ namespace DollarStairs
         {
             int number;
             
+            //No error message when the number is not valid
             Console.Write("Enter a number: ");
             if (int.TryParse(Console.ReadLine(), out number))
             {
+                //Nice addition
                 Console.Write("Enter a character: ");
                 var ch = Console.ReadLine();
 
                 while (number > 0)
                 {
+                    //You are printing the pyramid upside down
                     for (int i = number; i > 0; i--)
                         Console.Write(ch);
+
                     Console.WriteLine();
                     number--;
                 }
