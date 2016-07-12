d3e0910b1fef5bb3aba18a67376a1d6156686800
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-12 16:29:55 +0300

    Lab 2.5

--- MulBoard/MulBoard/Program.cs
+++ MulBoard/MulBoard/Program.cs
@@ -14,6 +14,9 @@ namespace MulBoard
             {
                 for (int column = 1; column < 11; column++)
                 {
+                    //1. Console.Write doesn't need string.Format. It has an overload that get's both a format and an arguments.
+                    //2. Why are you using the '+' string operator inside a string.Fomat?
+                    //3. In C# we usually use the Alias string instead of String.
                     Console.Write(String.Format("{0,3} "+ "|",row*column));
                 }
                 Console.WriteLine("");
