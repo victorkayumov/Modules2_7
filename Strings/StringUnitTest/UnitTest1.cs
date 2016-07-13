8dfb539a1f9278f890f6da335215bfcdc48bda23
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-13 17:20:22 +0300

    Lab 4.1

--- Strings/StringUnitTest/UnitTest1.cs
+++ Strings/StringUnitTest/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace StringUnitTest
     [TestClass]
     public class UnitTest1
     {
+        //Should add more tests
         [TestMethod]
         public void Check_ReverseString_IsEqual()
         {
@@ -18,6 +19,7 @@ namespace StringUnitTest
             StringAssert.StartsWith("here was kayumov victor", splitStr.ReverseStr(str));
         }
 
+        //You should add more tests for unusuall cases like when an empty string is inputed, when only whitepsaces are inputed and so on
         [TestMethod]
         public void Check_LengthOfString_IsTrue()
         {         
@@ -27,6 +29,8 @@ namespace StringUnitTest
             Assert.AreEqual(4,len.SplitingString(strLen));
         }
 
+        //What about strings with the same words? like "a a a", white about numbers? "0111 1110" ? What about empty arrays?
+        //You should add more tests.
         [TestMethod]
         public void Check_SortedString_InRightPlace()
         {
--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -18,25 +18,35 @@ namespace Strings
                 var split = new Split();
                 split.SplitingString(str);
                 
+                //Why didn't you use string.isNullOrEmpty? You used it in other place.
             } while (str != "");
         }
 
     }
 
+    //usually it is better to extract the class to a different file(s)
     public class Split
     {
+        //the name of the method is 'SplittingString' but it does a lot more than that. It split strings, print them and then reverse and sort them.
+        //Try to find better names for methods
         public int SplitingString(string str)
         {
-
+            //Good. Also, look at string.IsNullOrWhiteSpace
             if (string.IsNullOrEmpty(str))
                 return 0 ;
+            //The else is not neccessary
             else
             {
-                string[] splitStr = Regex.Split(str, " ");
+                //This doesnn't handle multiple whitespaces, or even tabs.
+                //The regular expression pattern you used isn't optimal.
+                //Look at the following pattern "\s+". It will match multiple whitespaces and it will solve your problem
+                string[] splitStr = Regex.Split(str, @"\s+");
                 foreach (var split in splitStr)
                 {
                     Console.WriteLine(split);
                 }
+
+                //Nice use of the new string interpolation in C#
                 Console.WriteLine($"The number of words is: {splitStr.Length}");
 
                 var reverse = new ReverseString();
@@ -50,19 +60,26 @@ namespace Strings
        
     }
 
+    //usually it is better to extract such classes to seperate file(s)
     public class ReverseString
     {
         public string ReverseStr(string[] str)
         {
             Array.Reverse(str);
+
+            //Why didn't you return an array of strings? This approach is not good.
             return string.Join(" ", str);
         }
     }
+
+    //Well, this class might have been better as a method.
     public class Sort
     {
         public string SortingString(string[] str)
         {
             Array.Sort(str);
+
+            //Why didn't you return an array of strings, then printed the array? 
             return string.Join(" ", str);
         }
     }
